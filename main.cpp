cdaa0f693a024947868bb9a38c1cce80dbef4762
León G <A00573074@itesm.mx>
2022-10-24 02:51:19 +0000

    New

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ struct Point {
 
 //Función que imprime en consola un salto de línea, no recibe parámetros y no tiene valor de retorno.
 void espacio() { //Complejidad Computacional: O(1), es una ejecución lineal en el contenido de la función.
-    cout << endl;
+	cout << endl;
 }
 
 //Punto global que funciona como auxiliar para realizar un ordenamiento.
@@ -90,7 +90,7 @@ int compare(const void* vp1, const void* vp2) { //Complejidad Computacional: O(1
 }
 
 //Función que imprime la cáscara conveza dado un arreglo de puntos, recibe además la cantidad de puntos, no tiene valor de retorno.
-void convexHull(Point points[], int n) { //Complejidad Computacional: O(n). 
+void convexHull(Point points[], int n) { //Complejidad Computacional: O(nlogn).
 
 	int ymin = points[0].y, min = 0;
 
@@ -105,7 +105,7 @@ void convexHull(Point points[], int n) { //Complejidad Computacional: O(n).
 
 	swap(points[0], points[min]);
 	p0 = points[0];
-	qsort(&points[1], n - 1, sizeof(Point), compare);
+	qsort(&points[1], n - 1, sizeof(Point), compare); //Complejidad Computacional: O(nlogn).
 
 	int m = 1;
 
@@ -119,7 +119,9 @@ void convexHull(Point points[], int n) { //Complejidad Computacional: O(n).
 	}
 
 	if (m < 3) {
+		espacio();
 		cout << "¡¡¡IMPOSIBLE LA CREACION DE UN POLIGONO CONVEXO!!!";
+		espacio();
         espacio();
 		return;
 	}
@@ -144,6 +146,7 @@ void convexHull(Point points[], int n) { //Complejidad Computacional: O(n).
 		S.pop();
 	}
 
+	espacio();
 	cout << "POLIGONO CONVEXO MAS PEQUENIO: [PUNTOS DADOS EN SENTIDO CONTRARIO AL RELOJ]" << endl;
 
 	for (int i = result.size() - 1; i >= 0; i--) {
@@ -151,13 +154,15 @@ void convexHull(Point points[], int n) { //Complejidad Computacional: O(n).
 		espacio();
 	}
 
+	espacio();
+
 }
 
 //Función main que ejecuta el programa, no recibe parámetros, retorna un entero [return 0].
-int main() { //Complejidad Computacional: O(1). Dentro de esa ejecución lineal, presenta un ciclo for de complejidad O(n).
-	
+int main() { //Complejidad Computacional: O(1). Dentro de esa ejecución lineal, el emplear el Graham Scan para la creación de la cáscara, se encuentra una complejidad de: O(nlogn).
+
 	int n;
-	
+
 	cout << "INGRESE LA CANTIDAD DE PUNTOS: " << endl;
 	cin >> n;
 
@@ -167,9 +172,9 @@ int main() { //Complejidad Computacional: O(1). Dentro de esa ejecución lineal,
 		int xCord;
 		int yCord;
 		Point auxiliar;
-		cout << "x" << i+1 << ": " << endl;
+		cout << "x" << i + 1 << ": ";
 		cin >> xCord;
-		cout << "y" << i + 1 << ": " << endl;
+		cout << "y" << i + 1 << ": ";
 		cin >> yCord;
 
 		auxiliar.x = xCord;
@@ -177,7 +182,7 @@ int main() { //Complejidad Computacional: O(1). Dentro de esa ejecución lineal,
 		points[i] = auxiliar;
 	}
 
-	convexHull(points, n);
+	convexHull(points, n); //Complejidad Computacional: O(nlogn).
 
 	return 0;
 }
